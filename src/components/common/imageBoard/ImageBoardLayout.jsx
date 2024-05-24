aa9a361e56c26d1b3add6ff417cc12a3a50f23cc
Hyunjun-11 <mical0108@gmail.com>
2024-05-25 06:09:46 +0900

    feat : imageBoardLayout

---
+++ src/components/common/imageBoard/ImageBoardLayout.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import "./ImageBoardLayout.css";
+
+const ImageBoardLayout = ({ title, imageList }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (id) => {
+    navigate(`/image/board/${id}`);
+  };
+
+  return (
+    <div className="ImageBoardLayout">
+      <div>{title}</div>
+      <div className="image_board">
+        {imageList.map((item) => (
+          <div key={item.id} className="image_item" onClick={() => handleClick(item.id)}>
+            <img src={item.url} alt={item.title} className="image_thumbnail" />
+            <div className="image_title">{item.title}</div>
+            <div className="image_date">{item.date}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageBoardLayout;
