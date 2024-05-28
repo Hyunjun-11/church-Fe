5d635dfefa2ed3a5586a9f0029508f7bfca06848
Hyunjun-11 <mical0108@gmail.com>
2024-05-28 17:01:16 +0900

    feat : body Layout banner

--- src/components/bodyTemp/layout/Layout.jsx
+++ src/components/bodyTemp/layout/Layout.jsx
@@ -5,11 +5,14 @@ import { Outlet } from "react-router-dom";
 const Layout = ({ ListComponent }) => {
   return (
     <div className="Layout">
-      <div className="List">
-        <ListComponent type={"LIST"} />
-      </div>
-      <div className="Outlet">
-        <Outlet />
+      <div className="Banner">배너공간</div>
+      <div className="BodyTemp">
+        <div className="List">
+          <ListComponent type={"LIST"} />
+        </div>
+        <div className="Outlet">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
