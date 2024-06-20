43cb843fa5aca34e990273117ec5753bdda44cf5
Hyunjun-11 <mical0108@gmail.com>
2024-06-20 18:16:23 +0900

    feat : adminPage board

--- src/Pages/admin/AdminPost.jsx
+++ src/Pages/admin/AdminPost.jsx
@@ -1,9 +1,150 @@
+import React, { useEffect, useRef, useState } from "react";
+import BodyTitle from "../../components/common/BodyTitle";
+import styled from "styled-components";
+import Category from "../../components/common/Category";
+import api from "../../api/api";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const AdminPost = () => {
+  const location = useLocation();
+  const nav = useNavigate();
+  const [boards, setBoards] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(
+    location.state?.category || "전체"
+  );
+  const [selectedParam, setSelectedParam] = useState("");
+
+  const categoryList = [
+    { name: "전체", param: "" },
+    { name: "다니엘 부서", param: "DANIEL" },
+    { name: "사무엘 부서", param: "SAMUEL" },
+    { name: "청년부 부서", param: "YOUTH" },
+    { name: "여전도회 부서", param: "WOMEN" },
+    { name: "남전도회 부서", param: "MEN1" },
+    { name: "함섬 갤러리", param: "GALLERY" },
+    { name: "함섬 영상갤러리", param: "VIDEO_GALLERY" },
+  ];
+
+  const handleCategoryChange = (category) => {
+    const selected = categoryList.find((cat) => cat.name === category);
+    setSelectedCategory(category);
+    setSelectedParam(selected ? selected.param : "");
+  };
+
+  useEffect(() => {
+    const fetchCategoryBoard = async () => {
+      try {
+        const endpoint =
+          selectedCategory === "전체"
+            ? "board/"
+            : `board/category?category=${selectedParam}`;
+        const response = await api.get(endpoint);
+        const sortedList = response.data.data.sort(
+          (a, b) => new Date(b.createAt) - new Date(a.createAt)
+        );
+        setBoards(sortedList);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchCategoryBoard();
+  }, [selectedCategory, selectedParam]);
+
+  const handleClick = (id) => {
+    nav(`${id}`, { state: { category: selectedCategory } });
+  };
+
   return (
-    <div>
-      <h2>AdminPost</h2>
-    </div>
+    <Container>
+      <BodyTitle title={"게시물 관리"} />
+      <Category list={categoryList} onCategoryChange={handleCategoryChange} />
+      <Board>
+        <BoardHeader>
+          <BoardColumn className="number">번호</BoardColumn>
+          <BoardColumn className="title">제목</BoardColumn>
+          <BoardColumn className="author">작성자</BoardColumn>
+          <BoardColumn className="date">날짜</BoardColumn>
+        </BoardHeader>
+        {boards.map((item, index) => (
+          <BoardBody key={item.boardId}>
+            <BoardColumn className="number">{index + 1}</BoardColumn>
+            <BoardColumn
+              className="title"
+              onClick={() => handleClick(item.boardId)}>
+              {item.title}
+            </BoardColumn>
+            <BoardColumn className="author">{item.author}</BoardColumn>
+            <BoardColumn className="date">
+              {new Date(item.createAt).toLocaleString("ko-KR", {
+                timeZone: "Asia/Seoul",
+              })}
+            </BoardColumn>
+          </BoardBody>
+        ))}
+      </Board>
+    </Container>
   );
 };
 
 export default AdminPost;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Board = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+const BoardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const BoardBody = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #e8f1fa;
+  }
+`;
+
+const BoardColumn = styled.div`
+  text-align: center;
+  padding: 0 10px;
+
+  &.number {
+    width: 10%;
+  }
+
+  &.title {
+    width: 40%;
+    cursor: pointer;
+    text-align: left;
+  }
+
+  &.author {
+    width: 15%;
+  }
+
+  &.date {
+    width: 20%;
+  }
+  &.category {
+    width: 15%;
+  }
+`;
---
+++ src/Pages/admin/AdminPostDetail.jsx
@@ -0,0 +1,191 @@
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import api from "../../api/api";
+
+const BoardDetailContainer = styled.div`
+  padding: 20px;
+  //   height: 100%;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.02);
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 32px;
+  margin-bottom: 20px;
+  color: #333;
+`;
+
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+  color: #777;
+  margin-bottom: 40px; /* 제목과 작성자 사이의 간격을 늘리기 위해 변경 */
+`;
+
+const Author = styled.div`
+  padding-top: 20px;
+  font-weight: bold;
+`;
+
+const DateStyled = styled.div`
+  font-size: 14px;
+  color: #777;
+`;
+
+const Content = styled.div`
+  font-size: 18px;
+  line-height: 1.6;
+  color: #444;
+  padding: 20px 0;
+  border-top: 1px solid #ddd;
+  // border-bottom: 1px solid #ddd;
+  margin-bottom: 20px; /* 추가하여 아래 경계선이 맨 아래까지 나오도록 조정 */
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const LeftButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const RightButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Button = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+const BackButton = styled.button`
+  padding: 12px 20px;
+  background-color: transparent;
+  border: solid 1px;
+  border-radius: 8px;
+  color: #1976d2;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const AdminPostDetail = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const user = useSelector((state) => state.user);
+  const category = location.state?.category || "전체";
+
+  useEffect(() => {
+    const fetchBoardDetail = async () => {
+      try {
+        const response = await api.get(`/board/${id}`);
+        setSelectedItem(response.data.data);
+      } catch (error) {
+        setError("게시글을 불러오는데 실패했습니다.");
+        console.error("board detail! error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBoardDetail();
+  }, [id]);
+
+  const handleEditClick = () => {
+    if (selectedItem && user && selectedItem.memberId === user.id) {
+      navigate(`/test/board/edit/${id}`);
+    } else {
+      alert("작성자만 수정할 수 있습니다.");
+    }
+  };
+
+  const handleDeleteClick = async () => {
+    try {
+      if (selectedItem && user && selectedItem.memberId === user.id) {
+        await api.delete(`board/${id}`);
+        alert("게시글이 삭제되었습니다.");
+        navigate(`/admin/posts`, { state: { category } });
+      } else {
+        alert("작성자만 삭제할 수 있습니다.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("게시글 삭제에 실패했습니다.");
+    }
+  };
+
+  const handleBackClick = () => {
+    console.log(category);
+    navigate(`/admin/posts`, { state: { category } });
+  };
+
+  if (loading) return <div>로딩 중...</div>;
+  if (error) return <div>{error}</div>;
+
+  const formatDate = (dateString) => {
+    const date = new window.Date(dateString);
+    return date.toLocaleString();
+  };
+  return (
+    <>
+      <BoardDetailContainer>
+        {selectedItem ? (
+          <>
+            <Title>{selectedItem.title}</Title>
+            <Info>
+              <Author>작성자: {selectedItem.author}</Author>
+              <DateStyled>{formatDate(selectedItem.createAt)}</DateStyled>
+            </Info>
+            <Content
+              dangerouslySetInnerHTML={{ __html: selectedItem.content }}
+            />
+          </>
+        ) : (
+          <div>게시글을 찾을 수 없습니다.</div>
+        )}
+      </BoardDetailContainer>
+      {selectedItem && (
+        <ButtonContainer>
+          <LeftButtonContainer>
+            <BackButton onClick={handleBackClick}>목록으로</BackButton>
+          </LeftButtonContainer>
+          <RightButtonContainer>
+            <Button onClick={handleEditClick}>수정</Button>
+            <Button onClick={handleDeleteClick}>삭제</Button>
+          </RightButtonContainer>
+        </ButtonContainer>
+      )}
+    </>
+  );
+};
+
+export default AdminPostDetail;
