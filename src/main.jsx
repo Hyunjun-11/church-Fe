6d82f21450bf1205e066841194b63b23ecb40aac
Hyunjun-11 <mical0108@gmail.com>
2024-06-20 11:29:29 +0900

    feat : chart js add

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import ReactModal from "react-modal";
 import store from "./Store/store";
 import App from "./App";
 import "./index.css";
-
+import { Chart as ChartJS, registerables } from "chart.js";
 // 모달이 속한 앱의 루트 DOM 요소를 지정
 ReactModal.setAppElement("#root");
-
+ChartJS.register(...registerables);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
