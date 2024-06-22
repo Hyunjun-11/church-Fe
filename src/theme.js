5944fe6f08110faa8259c613cf01a9e294da7fd5
Hyunjun-11 <mical0108@gmail.com>
2024-06-22 12:16:12 +0900

    feat : miniCalendar

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,15 @@
 export const theme = {
   primaryColor: "#007bff",
   titleBackgroundColor: "#007bff", // 추가된 배경색
+  hover: "#e8f1fa",
   dropdownBackgroundColor: "#007bff",
   dropdownHoverColor: "#007bff",
+  gray_1: "#333",
+  gray_5: "#ccc",
+  red_1: "#f00",
+  primary_2: "#00f",
+  primary_3: "#007bff",
+  yellow_2: "#ff0",
+  darkBlack: "#000",
+  br_2: "#555",
 };
