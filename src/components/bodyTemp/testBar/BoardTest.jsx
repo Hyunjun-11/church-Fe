aa88492abc006af73ca4804de49d6fcc793a3a10
Hyunjun-11 <mical0108@gmail.com>
2024-07-02 21:01:59 +0900

    chore : dependencies add

--- src/components/bodyTemp/testBar/BoardTest.jsx
+++ src/components/bodyTemp/testBar/BoardTest.jsx
@@ -1,11 +1,26 @@
+import styled from "styled-components";
 import BoardLayout from "../../common/board/BoardLayout";
 
 const BoardTest = () => {
   return (
     <div>
       <BoardLayout title={"개발 과정"} category={"DEVELOP"} />
+      <EditorInput type="text" placeholder="Type something here..." />
     </div>
   );
 };
 
 export default BoardTest;
+
+const EditorInput = styled.input`
+  width: 10ch; /* 초기 너비 설정 */
+  min-width: 10ch; /* 최소 너비 설정 */
+  max-width: 100%; /* 최대 너비 설정 */
+  box-sizing: content-box;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  display: inline-block;
+  overflow: hidden;
+  white-space: nowrap;
+`;
